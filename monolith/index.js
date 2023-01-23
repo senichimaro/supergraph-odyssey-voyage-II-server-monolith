beb37523767aad75b7d73c6d0d72dc569a17ceb4
senichi <senichimaro@gmail.com>
2023-01-23 12:48:50 +0100

    migration: step 1. monolith to subgraph - @apollo/subgraph + sserver schema:buildSubgraphSchema

--- monolith/index.js
+++ monolith/index.js
@@ -1,6 +1,6 @@
 const { ApolloServer } = require('@apollo/server');
 const { startStandaloneServer } = require('@apollo/server/standalone');
-
+const { buildSubgraphSchema } = require("@apollo/subgraph");
 const { readFileSync } = require('fs');
 const axios = require('axios');
 const gql = require('graphql-tag');
@@ -17,12 +17,12 @@ const AccountsAPI = require('./datasources/accounts');
 const PaymentsAPI = require('./datasources/payments');
 
 async function startApolloServer() {
-  const server = new ApolloServer({
+  const server = new ApolloServer({schema: buildSubgraphSchema({
     typeDefs,
     resolvers,
-  });
+  })});
 
-  const port = 4000;
+  const port = 4001;
 
   try {
     const { url } = await startStandaloneServer(server, {
